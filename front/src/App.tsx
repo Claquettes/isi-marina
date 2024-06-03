c342b0f3ea6506745b2708dfc0242e03c2922a33
claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2024-06-03 16:24:33 +0200

    feat: basic fetching of boats using the api

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import {Boat, user, emplacement} from '../model/types'
+
+
 import './App.css'
 
 function App() {
-  return (
-    <>
-     <div>
-         <h2>
-             Hello!!!!
-         </h2>
-     </div>
-    </>
-  )
+    const [boats, setBoats] = useState<Boat[]>([])
+
+    useEffect(() => {
+        fetch('http://146.59.233.238:3000/marina/boats')
+            .then(response => response.json())
+            .then(data => setBoats(data))
+    }
+    , [])
+
+    return (
+        <>
+            <div>
+                <h2>
+                    These are the boats:
+                </h2>
+                <ul>
+                    {boats.map(boat => (
+                        <li key={boat.id}>
+                            {boat.name} - {boat.length} - {boat.width} - {boat.year_of_immatriculation} - {boat.year_since_in_marina} - {boat.year_until_concession_payed} - {boat.id_owner}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default App
