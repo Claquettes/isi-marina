0644c4b8091561a4fdb409994a77e6d48d6b9a27
claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2024-06-03 16:09:12 +0200

    feat(added dto types)

---
+++ front/model/types.ts
@@ -0,0 +1,30 @@
+//we define the type Boat
+export type Boat = {
+    id: number
+    name: string
+    length: number
+    width: number
+    year_of_immatriculation: number
+    year_since_in_marina: number
+    year_until_concession_payed: number
+    id_owner: number
+}
+
+export type user = {
+    id: number
+    name: string
+    surname: string
+    third_name: string
+    year_of_birth: number
+    year_since_adherent: number
+}
+
+export type emplacement = {
+    id: number
+    e_row: number
+    e_column: number
+    price_per_year: number
+    year_of_attribution: number
+    id_owner: number
+    id_boat: number
+}
